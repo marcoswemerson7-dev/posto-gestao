2ce8ffc98688f14044d818e3c9e31f8f12b1a38f
marcoswemerson7-dev <marcoswemerson7@gmail.com>
2026-08-05 11:22:46 -0300

    Aplica correção definitiva da marca no app principal

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './brand.css';
+import './brandPatch';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
