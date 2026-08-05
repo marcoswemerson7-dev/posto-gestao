8ee79b146a2f680ecf9c7100d011468bfa266017
marcoswemerson7-dev <marcoswemerson7@gmail.com>
2026-08-05 12:27:36 -0300

    Remove import inválido do brandPatch

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './brand.css';
-import './brandPatch';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
